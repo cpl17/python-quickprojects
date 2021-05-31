db28966a49f8faac95955bd17f06a310d190240c
cpl17 <52930847+cpl17@users.noreply.github.com>
2021-05-31 14:40:41 -0400

    Update main.py

--- API/StockTracker/main.py
+++ API/StockTracker/main.py
@@ -72,7 +72,7 @@ BRIEF: {}
 if percent_diff > 0:
     chg_symbol = "🔺"
 else:
-    chg_symbol = "🔺"
+    chg_symbol = "🔻"
 
 
 if high_fluctuation:
@@ -88,42 +88,3 @@ if high_fluctuation:
                         from_='+15154686216',
                         to='+13028240859'
                 )
-   
-
-
-
-
-
-
-    
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#Optional: Format the SMS message like this: 
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
-
